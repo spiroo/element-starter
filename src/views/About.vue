f5997148bca40cf12c4205f209b66882bbd68302
spiroo <spiroo@126.com>
2019-02-19 19:41:11 +0800

    add theme

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+    </el-row>
   </div>
 </template>
