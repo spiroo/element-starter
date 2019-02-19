15a9cea5e289d368e4f80e8e59efffdb1c79c150
spiroo <spiroo@126.com>
2019-02-19 19:24:22 +0800

    init project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,29 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+import ElementUI from 'element-ui';
+import Cookies from 'js-cookie';
+import 'element-ui/lib/theme-chalk/index.css';
+
+import '@/styles/index.scss';
+import '@/icons';
+
+import common from '@/utils/common'; // 全局方法
+import filters from '@/utils/filters'; // 全局过滤器
+import '@/utils/permission';
+
+Vue.use(ElementUI, {
+  size: Cookies.get('size') || 'medium' // set element-ui default size
+});
+
+// 注册过滤器
+for (let key in filters) {
+  Vue.filter(key, filters[key]);
+}
+
+Vue.use(common);
 
 Vue.config.productionTip = false;
 
@@ -9,4 +31,4 @@ new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app');
